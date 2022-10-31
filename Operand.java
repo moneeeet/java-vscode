06f16b1c3cb20524576162e8cc896806bfc770d7
moneeeet <t.o.p11040809@gmail.com>
2022-11-01 01:17:56 +0900

    first commit

---
+++ Operand.java
@@ -0,0 +1,6 @@
+public class Operand {
+    public static void main(String[] args) {
+        String number = "1" ;
+        System.out.println(number);
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[] args) {
+        int myAge;
+        myAge = 26;
+        myAge = 29;
+        System.out.println(myAge);
+    }
+}
