3b064a08f7bd3297d619c7d448aad93f48c800f1
moneeeet <t.o.p11040809@gmail.com>
2022-11-03 22:59:20 +0900

    型変換

--- ConvertVar.java
+++ ConvertVar.java
@@ -1,7 +1,10 @@
 public class ConvertVar {
     public static void main(String[] args) {
         int number1 = 100;
-        long number2 = 100;
+        long number2 = 200;
+        System.out.println("number1:" + number1);
+        System.out.println("number2:" + number2);
+        number2 = number1; 
         System.out.println("number1:" + number1);
         System.out.println("number2:" + number2);
     }
