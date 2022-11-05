b24c74fd5e2f4f42594bdae907990ada13987b43
moneeeet <t.o.p11040809@gmail.com>
2022-11-05 23:53:21 +0900

    エスケープシーケンス

--- EscapeTest.java
+++ EscapeTest.java
@@ -5,6 +5,9 @@ public class EscapeTest {
         char c = '\''; //char型のシングルクオテーションでもバックスラッシュを使ってエスケープさせて表示させることができる
         System.out.println("c!");
         System.out.println("\\ is desplayed."); //バックスラッシュ２つで１つ表示させることができる
+        System.out.println("Hello!\n World!!"); //バックスラッシュとnで改行の制御文字
+        System.out.println("Hello!\t World!!"); //バックスラッシュとt（tab）で余白を入れることができる　TABの制御文字
     }
+
     
 }
