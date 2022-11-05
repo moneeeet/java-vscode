c4c6b86b528a85aac935eb3e7274866b50294a91
moneeeet <t.o.p11040809@gmail.com>
2022-11-05 19:01:44 +0900

    エスケープシーケンス

--- EscapeTest.java
+++ EscapeTest.java
@@ -1,7 +1,10 @@
 public class EscapeTest {
     public static void main (String[] args) {
         System.out.println("Hello!!");
-        System.out.println("Double quote");
+        System.out.println("\"Double quote(\") is desplayed."); //バックスラッシュ（＼ ooptionと￥マーク）でエスケープする。"は文字列を囲むという意味だけど、＼をすることによってエスケープさせることができる
+        char c = '\''; //char型のシングルクオテーションでもバックスラッシュを使ってエスケープさせて表示させることができる
+        System.out.println("c!");
+        System.out.println("\\ is desplayed."); //バックスラッシュ２つで１つ表示させることができる
     }
     
 }
