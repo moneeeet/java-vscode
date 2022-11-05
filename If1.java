ca53f63247da016ed292a3a974337c278e574325
moneeeet <t.o.p11040809@gmail.com>
2022-11-06 00:09:45 +0900

    if文条件分岐

---
+++ If1.java
@@ -0,0 +1,10 @@
+public class If1 {
+    public static void main(String[] args) {
+        int myAge = 34;
+        if(myAge == 35) {
+            System.out.println("私の年齢は35歳！");
+        }
+        System.out.println("私の出身地は・・・");
+    }
+    
+}
