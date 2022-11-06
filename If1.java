3153935ac9bbd1a34841c948096c46d6e04c570e
moneeeet <t.o.p11040809@gmail.com>
2022-11-06 21:53:09 +0900

    else追加

--- If1.java
+++ If1.java
@@ -1,8 +1,10 @@
 public class If1 {
     public static void main(String[] args) {
-        int myAge = 34;
+        int myAge = 35;
         if(myAge == 35) {
             System.out.println("私の年齢は35歳！");
+        }else {
+            System.out.println("私の年齢は35歳ではないです！");
         }
         System.out.println("私の出身地は・・・");
     }
