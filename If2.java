d115277778959cbabba62e99f980e6a71564eec2
moneeeet <t.o.p11040809@gmail.com>
2022-11-07 16:36:46 +0900

    if文練習

---
+++ If2.java
@@ -0,0 +1,14 @@
+public class If2 {
+    public static void main(String[] args) {
+        int myAge = 33;
+        if(myAge == 35) {
+            System.out.println("私の年齢は35歳です！");
+        } else if(myAge >= 40) {
+            System.out.println("私の年齢は40歳以上です！");
+        } else {
+            System.out.println("私の年齢は35歳でも40歳以上でもないです！");
+        }
+        System.out.println("私の出身地は・・・");
+        
+    }
+}
