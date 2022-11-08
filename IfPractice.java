3141ca2bcd21b07a00091d0fdf12ddcca1e9912c
moneeeet <t.o.p11040809@gmail.com>
2022-11-08 22:45:34 +0900

    if文練習

---
+++ IfPractice.java
@@ -0,0 +1,10 @@
+public class IfPractice {
+    public static void main(String[] args) {
+        String myHometown = "Osaka";
+        if(myHometown == "Tokyo")  {
+            System.out.println("私の出身地は東京です。");
+        }else {
+            System.out.println("私の出身地は東京ではありません。");
+        }
+    }
+}
