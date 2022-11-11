9a1d40b6d802cbd5d7459d85e3375034761f46d3
moneeeet <t.o.p11040809@gmail.com>
2022-11-11 10:48:46 +0900

    final飾子

---
+++ Chapter4.java
@@ -0,0 +1,11 @@
+public class Chapter4 {
+    public static void main(String[] args) {
+        final int luckyNumber = 2;
+        String name = "Mone Hiyama";
+            System.out.println(name);
+        name = "ruruchan";
+            System.out.println(name);
+            System.out.println(luckyNumber);
+        
+    }
+}
