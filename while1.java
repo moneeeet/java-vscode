09cccc1c16ee74ce19ee34e2e3d60f7b056601da
moneeeet <t.o.p11040809@gmail.com>
2022-11-15 19:05:19 +0900

    while文

---
+++ while1.java
@@ -0,0 +1,10 @@
+public class while1 {
+    public static void main(String[] args) {
+        System.out.println("番号！");
+        int number = 1;
+        while(number <= 10) {
+            System.out.println(number + "!");
+            number++;
+        }
+    }
+}
