78c39289fbc9e0d91147554870f3efa847a7cb5b
moneeeet <t.o.p11040809@gmail.com>
2022-11-18 22:40:51 +0900

    for文・繰り返し処理

---
+++ For1.java
@@ -0,0 +1,9 @@
+public class For1 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            int n = i + 1;
+            System.out.println("繰り返し:" + n + "周目");
+
+        }
+    }
+}
