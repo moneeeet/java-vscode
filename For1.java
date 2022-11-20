14ab18a7a50ab4537eb9c111c4a7cc1b4fa7a212
moneeeet <t.o.p11040809@gmail.com>
2022-11-20 22:48:44 +0900

    for文でのbreak

--- For1.java
+++ For1.java
@@ -1,8 +1,19 @@
 public class For1 {
     public static void main(String[] args) {
-        for(int i = 0; i <= 10; i++) {
-            int n = i + 1;
-            System.out.println("繰り返し:" + n + "周目");
+        for(int i = 1; i <= 10; i++) {
+            //int n = i + 1;
+            //System.out.println("繰り返し:" + n + "周目");
+            /* 
+            if (i == 5) { この位置にbreakを記述したら４周目まで表示される
+                break;
+            }
+             */
+            System.out.println("繰り返し:" + i + "周目");
+            if (i == 5) { //ここなら５周目まで表示される
+                break;
+            }
+            
+                
         }
     }
 }
