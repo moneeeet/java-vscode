5dc6c2748dbd44581ca33f49c6e1b7d15cd48cd5
moneeeet <t.o.p11040809@gmail.com>
2022-11-22 23:23:16 +0900

    for文(break・continue)

--- For1.java
+++ For1.java
@@ -13,10 +13,15 @@ public class For1 {
                 System.out.println("次のループに進む");
                 continue; //次のループ（周）に進む。ここでは、５周目を飛ばして10周目まで表示される。continuw以下は実行せず、１行目に戻って実行し始める
             }
+            System.out.println("繰り返し:" + i + "周目");        
+        } 
+        System.out.println("新しいループ");
+        for(int i = 1; i <= 5; i++) {
+            //int n = i + 1;
+            if(i == 3){
+                break;
+            }
             System.out.println("繰り返し:" + i + "周目");
-            
-            
-                
         }
     }
 }
