60a9f7b3705883513339af6ce2a3989585524993
moneeeet <t.o.p11040809@gmail.com>
2022-11-25 21:54:51 +0900

    二重ループ

--- ForDouble.java
+++ ForDouble.java
@@ -1,10 +1,11 @@
 public class ForDouble {
     public static void main(String[] args) {
         for(int i = 1; i <= 2; i++) {
-            System.out.println("i:" + i);
+            System.out.print("【i:" + i + "】");
             for(int j = 1; j <= 3; j++) {
-                System.out.println("j:" + j);
+                System.out.print("j:" + j + " ");
             }
+            System.out.println();
         }
     }
 }
