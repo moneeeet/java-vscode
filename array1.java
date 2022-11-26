725c62c2af8dee3aa19056c34433c9b2359f572e
moneeeet <t.o.p11040809@gmail.com>
2022-11-26 21:42:02 +0900

    配列

---
+++ array1.java
@@ -0,0 +1,10 @@
+public class array1 {
+    public static void main(String[] args) {
+        String name = "鈴木太郎";
+        String email = "suzuki_taro";
+        String phone = "090-0000-0000";
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(phone);
+    }
+}
