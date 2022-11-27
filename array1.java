20a95a18bca8784ea85282eb985f99347c56bb28
moneeeet <t.o.p11040809@gmail.com>
2022-11-27 23:31:34 +0900

    配列

--- array1.java
+++ array1.java
@@ -1,4 +1,4 @@
-public class array1 {
+public class Array1 {
     public static void main(String[] args) {
         String name = "鈴木太郎";
         String email = "suzuki_taro";
@@ -6,5 +6,17 @@ public class array1 {
         System.out.println(name);
         System.out.println(email);
         System.out.println(phone);
+
+        System.out.println(); //カッコの中が空欄なら１行空行になる
+
+        String[] customer; //string型の配列を宣言
+        customer = new String[3]; 
+        customer[0] = "鈴木太郎"; 
+        customer[1] = "suzuki_taro@xxxx.com";
+        customer[2] = "090-0000-9999"; 
+        System.out.println(customer[0]);
+        System.out.println(customer[1]);
+        System.out.println(customer[2]);
+
     }
 }
