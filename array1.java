ad51f11304341685769de01b04ffd7aa3baa566b
moneeeet <t.o.p11040809@gmail.com>
2022-11-28 18:02:21 +0900

    配列

--- array1.java
+++ array1.java
@@ -10,10 +10,10 @@ public class Array1 {
         System.out.println(); //カッコの中が空欄なら１行空行になる
 
         String[] customer; //string型の配列を宣言
-        customer = new String[3]; 
-        customer[0] = "鈴木太郎"; 
-        customer[1] = "suzuki_taro@xxxx.com";
-        customer[2] = "090-0000-9999"; 
+        customer = new String[3]; //配列の初期化を宣言
+        customer[0] = "鈴木太郎"; //[0]で１番目。０から開始する。これを「添字」または「index」という。
+        customer[1] = "suzuki_taro@xxxx.com"; //[1]で２番目
+        customer[2] = "090-0000-9999"; //[2]で３番目
         System.out.println(customer[0]);
         System.out.println(customer[1]);
         System.out.println(customer[2]);
