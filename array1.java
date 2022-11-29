4ea6afe5f2e2db8d082cb4ded3ee6cc28583c221
moneeeet <t.o.p11040809@gmail.com>
2022-11-29 23:54:41 +0900

    配列

--- array1.java
+++ array1.java
@@ -18,5 +18,18 @@ public class Array1 {
         System.out.println(customer[1]);
         System.out.println(customer[2]);
 
+        System.out.println();
+
+    //    String customer2;
+    //    customer2 = {"鈴木太郎", "suzuki_taro@xxxx.com", "090-0000-9999"}; //{}で初期化する場合、宣言と初期化を分けられない
+    //    String[] customer2;
+    //    customer2 = new String[]{"鈴木太郎", "suzuki_taro@xxxx.com", "090-0000-9999"}; // new 型名[]{}で初期化する場合、宣言と初期化を分けられる
+        
+        String[] customer2 = new String[]{"鈴木太郎", "suzuki_taro@xxxx.com", "090-0000-9999"}; // new 型名[]で初期化する場合、宣言と初期化を同時に行う
+        System.out.println(customer2[0]);
+        System.out.println(customer2[1]);
+        System.out.println(customer2[2]);
+    } 
+
     }
-}
+
